03f4cd9cf638b035661f7fdd894fe61b6b0a493e
Girish <girishmroy143@gmail.com>
2020-12-08 16:18:37 +0530

    gitHub push commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NoteIcon from '@material-ui/icons/Note';
+
+function Heading() {
+
+    return (
+        <header><h1> <NoteIcon />NotePad</h1></header>
+    );
+    
+}
+
+export default Heading;
